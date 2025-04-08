505a443ef3a28d130a1fc3a8722716077df65417
duytan40400 <duytan40400@gmail.com>
2025-04-08 22:40:10 +0700

    Delete .puppeteerrc.cjs

--- .puppeteerrc.cjs
+++
@@ -1,9 +0,0 @@
-const { join } = require('path');
-
-/**
- * @type {import("puppeteer").Configuration}
- */
-module.exports = {
-    // Thay đổi thư mục cache cho Puppeteer
-    cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
-};
